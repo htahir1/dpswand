removed bug of prediction calculation

app.py
@@ -24,7 +24,7 @@ class Person(db.Model):
 
 class Gesture(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Unicode, unique=True)
+    name = db.Column(db.Unicode)
     raw_data = db.Column(db.String(300))
     owner_id = db.Column(db.Integer, db.ForeignKey('person.id'))
     owner = db.relationship('Person', backref=db.backref('gestures',
@@ -39,7 +39,9 @@ class Gesture(db.Model):
 
 
 def convert_gesture_raw_to_np(raw_data):
+    raw_data = raw_data.replace('\r', "")
     samples = [sample.split(', ') for sample in raw_data.split('\n')]
+
     return np.array(samples, dtype=float).reshape(-1, 1)
 
 
@@ -94,10 +96,10 @@ def post_test_gesture():
             results = []
             for gesture in gestures:
                 template = convert_gesture_raw_to_np(gesture.raw_data)
-                dist, cost, acc, path = predictor.calculate_error(template=template, test_data=test)
+                dist = predictor.calculate_error(template=template, test_data=test)
                 results.append({'name' : gesture.name, 'dist' : dist})
 
-    return render_template('prediction_result.html', errors=errors, prediction=results)
+    return render_template('prediction_result.html', errors=errors, predictions=results)
 
 
 if __name__ == '__main__':

common/DTW.py
@@ -1,6 +1,6 @@
-from dtw import dtw
 import numpy as np
 import matplotlib.pyplot as plt
+from dtw import dtw
 
 
 class DynamicTimeWarping:
@@ -9,16 +9,18 @@ class DynamicTimeWarping:
     def __init__(self):
         self.dist = 0
         self.cost = []
-        self.acc = []
         self.path = []
 
     def calculate_error(self, template, test_data):
-        self.dist, self.cost, self.acc, self.path = dtw(template, test_data, dist=lambda x, y: np.linalg.norm(x - y, ord=1))
+        self.dist, self.cost, self.acc, self.path = dtw(template, test_data,
+                                                        dist=lambda x, y: np.linalg.norm(x - y, ord=1))
         return self.dist
 
     def make_plot(self):
-        plt.imshow(self.acc.T, origin='lower', cmap=plt.cm.gray, interpolation='nearest')
-        plt.plot(self.path[0], self.path[1], 'w')
-        plt.xlim((-0.5, self.acc.shape[0]-0.5))
-        plt.ylim((-0.5, self.acc.shape[1]-0.5))
+        fig = plt.figure(1)
+        ax = fig.add_subplot(111)
+        plot1 = plt.imshow(self.cost.T, origin='lower', cmap=plt.cm.gray, interpolation='nearest')
+        plot2 = plt.plot(self.path[0], self.path[1], 'w')
+        xlim = ax.set_xlim((-0.5, self.cost.shape[0] - 0.5))
+        ylim = ax.set_ylim((-0.5, self.cost.shape[1] - 0.5))
         plt.show()